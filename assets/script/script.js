3a04e4a16999dd8ccc8478d87e602f62eff00e7b
Cindy Paredes <cindyparedes@Cindys-MacBook-Pro-2.local>
2022-08-23 12:34:01 -0500

    added some local storage for the daily planner

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,42 @@
-$(document).ready();
-var today = moment()
-$("#currentDay").text(today.format("dddd, MMM Do"));
+var sevenAM = document.getElementById("seven");
+var saveDocument = document.getElementById("saveBtn");
+var sevenTime = "Seven AM:";
+var seven = sevenAM.value;
 
+sevenAM.innerHTML = localStorage.getItem(sevenTime);
+
+saveDocument.addEventListener("click", function (event) {
+  event.preventDefault(event);
+  const sevenTime = "Seven AM:";
+  const seven = sevenAM.value;
+
+  // console.log(time);
+  // console.log(reminder);
+
+  localStorage.setItem(sevenTime, seven);
+  location.reload();
+  sevenAM.innerHTML = localStorage.getItem(sevenTime);
+});
+
+// Start of 8AM
+var eightAM = document.getElementById("eight");
+var saveDocument2 = document.getElementById("saveBtn2");
+var eightTime = "Eigth AM:";
+var eight = eightAM.value;
+
+eightAM.innerText = localStorage.getItem(eightTime);
+
+saveDocument2.addEventListener("click", function (event) {
+  event.preventDefault(event);
+  var eightTime = "Eight AM:";
+  var eight = eightAM.value;
+
+  // console.log(eightTime);
+  // console.log(eight);
+
+  localStorage.setItem(eightTime, eight);
+  // location.reload();
+
+  eightAM.innerHTML = localStorage.getItem(eightTime);
+  console.log(localStorage.getItem(eightTime));
+});
