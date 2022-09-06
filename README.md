308289910e1be21d396bc3827f092ff98e6db058
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-09-05 20:32:05 -0500

    added the final sections for each section of the planner

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Work Day Scheduler Starter Code
+# PLAN-PLAN-Ahead
+
+Description: The following daily planner allows you to save certain information that is needed to be completed before a certain time. The planner will  display red when time has passed, the planner will display green when the hour is upon and needs to be completed, and finally the planner will display grey when the time has not passed. 
+
+Images:
+[](assets/Images/Screen%20Shot%202022-09-05%20at%208.23.42%20PM.png%0D) 
+[](assets/Images/Screen%20Shot%202022-09-05%20at%208.23.56%20PM.png%0D)
+
+
+Links:
--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@ var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 var CurrentTime = $("#currentTime").text(today.format("h:mm a"));
 var actualTime = today.hour()
-// console.log(actualTime);
+console.log(actualTime);
 
 // Start of 7am clock
 var sevenAM = $("#seven");
@@ -25,7 +25,9 @@ $(saveDocument).click(function (e) {
 });
 
 function itsSevenAm() {
-  if (actualTime === 7) {
+  if (actualTime < 7){
+    sevenAM.css("background", "grey");
+  } else if (actualTime === 7) {
     sevenAM.css("background", "limegreen");
   } else {
     sevenAM.css("background", "red");
@@ -35,7 +37,7 @@ itsSevenAm();
 
 // Start of 8am clock
 var eightAM = $("#eight");
-var saveDocument2 = $("#saveBtn3");
+var saveDocument2 = $("#saveBtn2");
 var eightTime = "Eight AM:";
 var eight = eightAM.val();
 // var myTime = today.format("h:mm:ss a");
@@ -53,7 +55,9 @@ $(saveDocument2).click(function (e) {
 });
 
 function itsEightAm() {
-  if (actualTime === 8) {
+  if (actualTime < 8){
+    sevenAM.css("background", "grey");
+  } else if (actualTime === 8) {
     eightAM.css("background", "limegreen");
   } else {
     eightAM.css("background", "red");
@@ -62,30 +66,433 @@ function itsEightAm() {
 itsEightAm();
 
 // Start of 9am clock
-var eightAM = $("#eight");
-var saveDocument2 = $("#saveBtn3");
-var eightTime = "Eight AM:";
-var eight = eightAM.val();
+var nineAM = $("#nine");
+var saveDocument3 = $("#saveBtn3");
+var nineTime = "Nine AM:";
+var nine = nineAM.val();
 // var myTime = today.format("h:mm:ss a");
 
-eightAM.text(localStorage.getItem(eightTime));
+nineAM.text(localStorage.getItem(nineTime));
 
-$(saveDocument2).click(function (e) { 
+$(saveDocument3).click(function (e) { 
   e.preventDefault(e);
-  const eightTime = "Eight AM:";
-  const eight = eightAM.val();
+  const nineTime = "Nine AM:";
+  const nine = nineAM.val();
 
  
-  localStorage.setItem(eightTime, eight);
-  eightAM.text(localStorage.getItem(eightTime));
+  localStorage.setItem(nineTime, nine);
+  nineAM.text(localStorage.getItem(nineTime));
 });
 
-function itsEightAm() {
-  if (actualTime === 8) {
-    eightAM.css("background", "limegreen");
+function itsNineAm() {
+  if (actualTime < 9){
+    nineAM.css("background", "grey");
+  } else if (actualTime === 9) {
+    nineAM.css("background", "limegreen");
   } else {
-    eightAM.css("background", "red");
+    nineAM.css("background", "red");
   }
 };
-itsEightAm();
+itsNineAm();
 
+// Start of 10am clock
+var tenAM = $("#ten");
+var saveDocument4 = $("#saveBtn4");
+var tenTime = "Ten AM:";
+var ten = tenAM.val();
+// var myTime = today.format("h:mm:ss a");
+
+tenAM.text(localStorage.getItem(tenTime));
+
+$(saveDocument4).click(function (e) { 
+  e.preventDefault(e);
+  const tenTime = "Ten AM:";
+  const ten = tenAM.val();
+
+ 
+  localStorage.setItem(tenTime, ten);
+  tenAM.text(localStorage.getItem(tenTime));
+});
+
+function itsTenAm() {
+  if (actualTime < 10){
+    tenAM.css("background", "grey");
+  } else if (actualTime === 10) {
+    tenAM.css("background", "limegreen");
+  } else {
+    tenAM.css("background", "red");
+  }
+};
+itsTenAm();
+
+
+// Start of 11am clock
+var elevenAM = $("#eleven");
+var saveDocument5 = $("#saveBtn5");
+var elevenTime = "Eleven AM:";
+var eleven = elevenAM.val();
+// var myTime = today.format("h:mm:ss a");
+
+elevenAM.text(localStorage.getItem(elevenTime));
+
+$(saveDocument5).click(function (e) { 
+  e.preventDefault(e);
+  const elevenTime = "Eleven AM:";
+  const eleven = elevenAM.val();
+
+ 
+  localStorage.setItem(elevenTime, eleven);
+  elevenAM.text(localStorage.getItem(elevenTime));
+});
+
+function itsElevenAm() {
+  if (actualTime < 11){
+    elevenAM.css("background", "grey");
+  } else if (actualTime === 11) {
+    elevenAM.css("background", "limegreen");
+  } else {
+    elevenAM.css("background", "red");
+  }
+};
+itsElevenAm();
+
+
+// Start of 12am clock
+var twelveAM = $("#twelve");
+var saveDocument6 = $("#saveBtn6");
+var twelveTime = "Twelve AM:";
+var twelve = twelveAM.val();
+// var myTime = today.format("h:mm:ss a");
+
+twelveAM.text(localStorage.getItem(twelveTime));
+
+$(saveDocument6).click(function (e) { 
+  e.preventDefault(e);
+  const twelveTime = "Twelve AM:";
+  const twelve = twelveAM.val();
+
+ 
+  localStorage.setItem(twelveTime, twelve);
+  twelveAM.text(localStorage.getItem(twelveTime));
+});
+
+function itsTwelveAm() {
+  if (actualTime < 12){
+    twelveAM.css("background", "grey");
+  } else if (actualTime === 12) {
+    twelveAM.css("background", "limegreen");
+  } else {
+    twelveAM.css("background", "red");
+  }
+};
+itsTwelveAm();
+
+
+// Start of 13pm clock
+var thirteenPM = $("#thirteen");
+var saveDocument7 = $("#saveBtn7");
+var thirteenTime = "Thirteen PM:";
+var thirteen = thirteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+thirteenPM.text(localStorage.getItem(thirteenTime));
+
+$(saveDocument7).click(function (e) { 
+  e.preventDefault(e);
+  const thirteenTime = "Thirteen PM:";
+  const thirteen = thirteenPM.val();
+
+ 
+  localStorage.setItem(thirteenTime, thirteen);
+  thirteenPM.text(localStorage.getItem(thirteenTime));
+});
+
+function itsThirteenPm() {
+  if (actualTime < 13){
+    thirteenPM.css("background", "grey");
+  } else if (actualTime === 13) {
+    thirteenPM.css("background", "limegreen");
+  } else {
+    thirteenPM.css("background", "red");
+  }
+};
+itsThirteenPm();
+
+
+// Start of 14pm clock
+var fourteenPM = $("#fourteen");
+var saveDocument8 = $("#saveBtn8");
+var fourteenTime = "Fourteen PM:";
+var fourteen = fourteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+fourteenPM.text(localStorage.getItem(fourteenTime));
+
+$(saveDocument8).click(function (e) { 
+  e.preventDefault(e);
+  const fourteenTime = "Fourteen PM:";
+  const fourteen = fourteenPM.val();
+
+ 
+  localStorage.setItem(fourteenTime, fourteen);
+  fourteenPM.text(localStorage.getItem(fourteenTime));
+});
+
+function itsFourteenPm() {
+  if (actualTime < 14){
+    fourteenPM.css("background", "grey");
+  } else if (actualTime === 14) {
+    fourteenPM.css("background", "limegreen");
+  } else {
+    fourteenPM.css("background", "red");
+  }
+};
+itsFourteenPm();
+
+
+// Start of 15pm clock
+var fifteenPM = $("#fifteen");
+var saveDocument9 = $("#saveBtn9");
+var fifteenTime = "Fifteen PM:";
+var fifteen = fifteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+fifteenPM.text(localStorage.getItem(fifteenTime));
+
+$(saveDocument9).click(function (e) { 
+  e.preventDefault(e);
+  const fifteenTime = "Fifteen PM:";
+  const fifteen = fifteenPM.val();
+
+ 
+  localStorage.setItem(fifteenTime, fifteen);
+  fifteenPM.text(localStorage.getItem(fifteenTime));
+});
+
+function itsFifteenPm() {
+  if (actualTime < 15){
+    fifteenPM.css("background", "grey");
+  } else if (actualTime === 15) {
+    fifteenPM.css("background", "limegreen");
+  } else {
+    fifteenPM.css("background", "red");
+  }
+};
+itsFifteenPm();
+
+
+// Start of 16pm clock
+var sixteenPM = $("#sixteen");
+var saveDocument10 = $("#saveBtn10");
+var sixteenTime = "Sixteen PM:";
+var sixteen = sixteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+sixteenPM.text(localStorage.getItem(sixteenTime));
+
+$(saveDocument10).click(function (e) { 
+  e.preventDefault(e);
+  const sixteenTime = "Sixteen PM:";
+  const sixteen = sixteenPM.val();
+
+ 
+  localStorage.setItem(sixteenTime, sixteen);
+  sixteenPM.text(localStorage.getItem(sixteenTime));
+});
+
+function itsSixteenPm() {
+  if (actualTime < 16){
+    sixteenPM.css("background", "grey");
+  } else if (actualTime === 16) {
+    sixteenPM.css("background", "limegreen");
+  } else {
+    sixteenPM.css("background", "red");
+  }
+};
+itsSixteenPm();
+
+
+// Start of 17pm clock
+var seventeenPM = $("#seventeen");
+var saveDocument11 = $("#saveBtn11");
+var seventeenTime = "Seventeen PM:";
+var seventeen = seventeenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+seventeenPM.text(localStorage.getItem(seventeenTime));
+
+$(saveDocument11).click(function (e) { 
+  e.preventDefault(e);
+  const seventeenTime = "Seventeen PM:";
+  const seventeen = seventeenPM.val();
+
+ 
+  localStorage.setItem(seventeenTime, seventeen);
+  seventeenPM.text(localStorage.getItem(seventeenTime));
+});
+
+function itsSeventeenPm() {
+  if (actualTime < 17){
+    seventeenPM.css("background", "grey");
+  } else if (actualTime === 17) {
+    seventeenPM.css("background", "limegreen");
+  } else {
+    seventeenPM.css("background", "red");
+  }
+};
+itsSeventeenPm();
+
+
+// Start of 18pm clock
+var eighteenPM = $("#eighteen");
+var saveDocument12 = $("#saveBtn12");
+var eighteenTime = "Eighteen PM:";
+var eighteen = eighteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+eighteenPM.text(localStorage.getItem(eighteenTime));
+
+$(saveDocument12).click(function (e) { 
+  e.preventDefault(e);
+  const eighteenTime = "Eighteen PM:";
+  const eighteen = eighteenPM.val();
+
+ 
+  localStorage.setItem(eighteenTime, eighteen);
+  eighteenPM.text(localStorage.getItem(eighteenTime));
+});
+
+function itsEighteenPm() {
+  if (actualTime < 18){
+    eighteenPM.css("background", "grey");
+  } else if (actualTime === 18) {
+    eighteenPM.css("background", "limegreen");
+  } else {
+    eighteenPM.css("background", "red");
+  }
+};
+itsEighteenPm();
+
+
+// Start of 19pm clock
+var nineteenPM = $("#nineteen");
+var saveDocument13 = $("#saveBtn13");
+var nineteenTime = "Nineteen PM:";
+var nineteen = nineteenPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+nineteenPM.text(localStorage.getItem(nineteenTime));
+
+$(saveDocument13).click(function (e) { 
+  e.preventDefault(e);
+  const nineteenTime = "Nineteen PM:";
+  const nineteen = nineteenPM.val();
+
+ 
+  localStorage.setItem(nineteenTime, nineteen);
+  nineteenPM.text(localStorage.getItem(nineteenTime));
+});
+
+function itsNineteenPm() {
+  if (actualTime < 19){
+    nineteenPM.css("background", "grey");
+  } else if (actualTime === 19) {
+    nineteenPM.css("background", "limegreen");
+  } else {
+    nineteenPM.css("background", "red");
+  }
+};
+itsNineteenPm();
+
+
+// Start of 20pm clock
+var twentyPM = $("#twenty");
+var saveDocument14 = $("#saveBtn14");
+var twentyTime = "Twenty PM:";
+var twenty = twentyPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+twentyPM.text(localStorage.getItem(twentyTime));
+
+$(saveDocument14).click(function (e) { 
+  e.preventDefault(e);
+  const twentyTime = "Twenty PM:";
+  const twenty = twentyPM.val();
+
+ 
+  localStorage.setItem(twentyTime, twenty);
+  twentyPM.text(localStorage.getItem(twentyTime));
+});
+
+function itsTwentyPm() {
+  if (actualTime < 20){
+    twentyPM.css("background", "grey");
+  } else if (actualTime === 20) {
+    twentyPM.css("background", "limegreen");
+  } else {
+    twentyPM.css("background", "red");
+  }
+};
+itsTwentyPm();
+
+
+// Start of 21pm clock
+var twentyonePM = $("#twentyone");
+var saveDocument15 = $("#saveBtn15");
+var twentyoneTime = "Twentyone PM:";
+var twentyone = twentyonePM.val();
+// var myTime = today.format("h:mm:ss a");
+
+twentyonePM.text(localStorage.getItem(twentyoneTime));
+
+$(saveDocument15).click(function (e) { 
+  e.preventDefault(e);
+  const twentyoneTime = "Twentyone PM:";
+  const twentyone = twentyonePM.val();
+
+ 
+  localStorage.setItem(twentyoneTime, twentyone);
+  twentyonePM.text(localStorage.getItem(twentyoneTime));
+});
+
+function itsTwentyonePm() {
+  if (actualTime < 21){
+    twentyonePM.css("background", "grey");
+  } else if (actualTime === 21) {
+    twentyonePM.css("background", "limegreen");
+  } else {
+    twentyonePM.css("background", "red");
+  }
+};
+itsTwentyonePm();
+
+
+// Start of 22pm clock
+var twentytwoPM = $("#twentytwo");
+var saveDocument16 = $("#saveBtn16");
+var twentytwoTime = "Twentytwo PM:";
+var twentytwo = twentytwoPM.val();
+// var myTime = today.format("h:mm:ss a");
+
+twentytwoPM.text(localStorage.getItem(twentytwoTime));
+
+$(saveDocument16).click(function (e) { 
+  e.preventDefault(e);
+  const twentytwoTime = "Twentytwo PM:";
+  const twentytwo = twentytwoPM.val();
+
+ 
+  localStorage.setItem(twentytwoTime, twentytwo);
+  twentytwoPM.text(localStorage.getItem(twentytwoTime));
+});
+
+function itsTwentytwoPm() {
+  if (actualTime < 22){
+    twentytwoPM.css("background", "grey");
+  } else if (actualTime === 22) {
+    twentytwoPM.css("background", "limegreen");
+  } else {
+    twentytwoPM.css("background", "red");
+  }
+};
+itsTwentytwoPm();
